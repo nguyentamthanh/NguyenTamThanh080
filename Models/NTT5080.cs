9be8cfa7e36379217ef5045553dfc47d51ef2768
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-10-06 08:12:34 +0700

    thanh

---
+++ Models/NTT5080.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NguyenTamThanh080.Models
+{
+    public class NTT5080
+    {
+        [Key]
+        [Required]
+        [StringLength(20)]
+        public string NVNId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string NVNName { get; set; }
+        public Boolean NVNGender { get; set; }
+    }
+}
